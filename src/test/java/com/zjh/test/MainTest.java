351a02e2c704d69a6f7adff910ec22c3a3461af6
pleasegivemeastar <1040756555@qq.com>
2019-12-02 15:28:55 +0800

    第二次提交

---
+++ src/test/java/com/zjh/test/MainTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.zjh.test;
+
+/**
+ * @author zjh
+ *
+ */
+public class MainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("nihao ");
+	}
+
+}
